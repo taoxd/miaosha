bb4b11bc7d66c4866f5611d44c2ebefa715d22b3
taoxudong <taoxudong@oraro.net>
2019-08-23 11:40:07 +0800

    otp验证码获取
    Change-Id: Ic009e13a499faa63e7c38d4631be9cde435c2691

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -13,12 +13,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
+/**
+ * @Description: 用户相关
+ * @Author: taoxudong
+ * @CreateDate: 2019/8/23 10:57
+ * @Version: 1.0
+ */
 @Controller("user")
 @RequestMapping("/user")
 public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    /**
+     * 通过Spring Bean包装的httpServletRequest，它的内部拥有ThreadLocal方式的map，
+     * 去让用户在每个线程中处理自己对应的request，并且有ThreadLocal清除的机制。
+     */
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    /**
+     * 用户获取otp短信接口
+     *
+     * @param telphone
+     */
+    @RequestMapping("/getOtp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+        //需要按照一定的规则生成OTP验证码
+        Random random = new Random();
+        //随机数范围[0,99999)
+        int randomInt = random.nextInt(99999);
+        //范围[10000,109999)
+        randomInt += 10000;
+
+        String otpCode = String.valueOf(randomInt);
+
+        //将OTP验证码同对应的用户的手机号关联,使用HttpSession的方式绑定 手机号 与 otpCode
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+        //将OTP验证码通过短信通道发送给用户,省略
+        System.out.println("telphone = " + telphone + " & otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+
+    }
+
 
     @RequestMapping("/get")
     @ResponseBody
